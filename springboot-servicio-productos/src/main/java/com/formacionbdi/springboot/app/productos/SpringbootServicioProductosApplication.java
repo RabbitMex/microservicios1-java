d35fb8a1a33955e5fe598b84ec5d409cec267478
Daniel Bastian Luna <green2718282@gmail.com>
2023-08-08 16:08:53 -0600

    Respaldo de proyecto productos

---
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -0,0 +1,14 @@
+package com.formacionbdi.springboot.app.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioProductosApplication {
+
+	public static void main(String[] args) {
+		//System.out.println("Iniciando Spring");
+		SpringApplication.run(SpringbootServicioProductosApplication.class, args);
+	}
+
+}
---
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.formacionbdi.springboot.app.productos.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
+
+/**
+ * 
+ */
+@RestController
+public class ProductoController {
+	@Autowired
+	private IProductoService productoService;
+	
+	//@Autowired
+	//private Environment env;
+	
+	@Value("${server.port}")
+	private Integer port;
+	
+	@GetMapping("/listar")
+	public List<Producto> listar(){
+		//System.out.println("ProductoController.listar()");
+		return productoService.findAll().stream().map(producto->{
+			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
+			return producto;
+		}).collect(Collectors.toList());
+		//return productoService.findAll();
+	}
+	
+	@GetMapping("/ver/{id}")
+	public Producto detalle(@PathVariable Long id) {
+		//System.out.println("ProductoController.detalle(): " + id);
+		Producto producto = productoService.findById(id);
+		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
+		return producto;
+	}
+}
